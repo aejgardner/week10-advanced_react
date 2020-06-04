13ae7b59723126bef0e43b978f20295fb74e5c4f
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-04 11:58:27 +0100

    Hook challenge 08 complete (PasswordStrength hook component)

---
+++ src/Hooks/PasswordStrength.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const RollCall = () => {
+
+    let [input, setInput] = useState("");
+
+    const handleChange = (e) => setInput(e.currentTarget.value);
+
+    let colour = "white";
+
+    if (input.length >= 1 && input.length < 9) {
+        colour = "red"
+    } else if (input.length >= 9 && input.length < 16) {
+        colour = "orange"
+    } else if (input.length >= 16) {
+        colour = "green"
+    }
+
+    return (
+        <input
+            style={{ backgroundColor: colour }}
+            type="password"
+            onChange={handleChange}
+            placeholder="Password strength tester"
+            value={input}
+            className="form-control container mt-4 mb-4 border border-secondary"
+        />
+    )
+};
+
+export default RollCall;
